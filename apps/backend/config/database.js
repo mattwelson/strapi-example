ec7b266392041180c1dd31a01bd850daedae7089
Matt Welson <matt@digitaltransformation.co.nz>
2024-07-03 17:12:12 +1200

    build: Dockerfile updates, and switched to pg for strapi

--- apps/backend/config/database.js
+++ apps/backend/config/database.js
@@ -1,11 +1,14 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env("DATABASE_CLIENT", "postgres"),
+
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Vw8O((JQbhD0~?)ICCRX0N)Hmujqi1e>"),
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
